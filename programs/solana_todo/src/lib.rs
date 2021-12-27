cacb1dc987b3d73e4016b2294ed946f678faf55b
Yunge Yu <yunge_yu@hotmail.co.nz>
2021-12-27 15:05:11 +1300

    backend with test

--- programs/solana_todo/src/lib.rs
+++ programs/solana_todo/src/lib.rs
@@ -5,10 +5,49 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_todo {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialise(ctx: Context<Initialise>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Update>, data: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.todo_list.push(data);
+        Ok(())
+    }
+
+    pub fn delete(ctx: Context<Update>, index: u8) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.todo_list.remove(index as usize);
+        Ok(())
+    }
+
+    pub fn complete(ctx: Context<Update>, index: u8) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = base_account.todo_list[index as usize].clone();
+        base_account.completed_list.push(copy);
+        base_account.todo_list.remove(index as usize);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialise<'info> {
+    #[account(init, payer=user, space=128+128)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub todo_list: Vec<String>,
+    pub completed_list: Vec<String>,
+}
--- tests/solana_todo.js
+++ tests/solana_todo.js
@@ -1,14 +1,91 @@
+const assert = require("assert");
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 describe('solana_todo', () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaTodo;
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  it("Initialise", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.SolanaTodo;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+    await program.rpc.initialise({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("initialised successfully");
+    assert.ok(account.todoList.length === 0);
+    assert.ok(account.completedList.length === 0);
+    _baseAccount = baseAccount;
+  });
+
+  it ("Add task", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.add("task one", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("added task: ", account.todoList.at(-1));
+    assert.ok(account.todoList.at(-1) === "task one");
+    assert.ok(account.todoList.length === 1);
+    assert.ok(account.completedList.length === 0);
+  });
+
+  it ("Add task 2", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.add("task two", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("added task: ", account.todoList.at(-1));
+    assert.ok(account.todoList.at(-1) === "task two");
+    assert.ok(account.todoList.length === 2);
+    assert.ok(account.completedList.length === 0);
+  });
+
+  it ("Complete task", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.complete(1, {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("completed task: ", account.completedList.at(-1));
+    assert.ok(account.completedList.at(-1) === "task two");
+    assert.ok(account.todoList.length === 1);
+    assert.ok(account.completedList.length === 1);
+  });
+
+  it ("Delete task", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.delete(0, {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("deleted succesfully");
+    assert.ok(account.todoList.length === 0);
+    assert.ok(account.completedList.length === 1);
   });
 });
